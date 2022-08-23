68d7afd801e3e4a8bce45aec3c789a0f89aed648
rizh09 <78652079+rizh09@users.noreply.github.com>
2022-08-23 18:45:15 +0800

    update the modifier of showToast

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 class ToasterMessage {
-    fun showToast(context: Context, msg: String, length : Int) {
+    public fun showToast(context: Context, msg: String, length: Int) {
         Toast.makeText(context, msg, length).show()
     }
 }
