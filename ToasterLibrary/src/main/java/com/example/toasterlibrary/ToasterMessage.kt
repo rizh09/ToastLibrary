2925d664ab00f9490e27b3536821a6a8d06b4e9b
rizh09 <78652079+rizh09@users.noreply.github.com>
2022-08-23 21:06:20 +0800

    declare function as top-level

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.kt
@@ -3,8 +3,6 @@ package com.example.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
-    public fun showToast(context: Context, msg: String, length: Int) {
-        Toast.makeText(context, msg, length).show()
-    }
+fun showToast(context: Context, msg: String, length: Int) {
+    Toast.makeText(context, msg, length).show()
 }
