29b198f62221d582958fbba80fe926aec6835eb5
rizh09 <78652079+rizh09@users.noreply.github.com>
2022-08-23 21:44:27 +0800

    add toasterLibrary 0.5.0 to app

--- app/src/main/java/com/example/toastlibrary/MainActivity.kt
+++ app/src/main/java/com/example/toastlibrary/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.toastlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.toasterlibrary.showToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast(baseContext,"hello from app 0.5.0", Toast.LENGTH_SHORT)
     }
 }
